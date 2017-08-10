b16b88e60aeb3875f8c7c4049e6d8a6ee2044a68
Muhammad Shidiq <emshidiq@gmail.com>
2017-08-10 20:47:18 +0700

    Update lists json response

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -9,14 +9,19 @@ use Validator;
 use DB;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 
 use App\Constant;
 
 use App\Models\Directory;
+use App\Models\Upload;
+use App\Models\Link;
 
 class ListsController extends Controller
 {
-
+    const PER_PAGE = 100;
+    const DEFAULT_PAGE = 1;
     const MSG_FOLDER_NOT_FOUND = 'Directory is not belongs to you!';
 
     /**
@@ -32,13 +37,70 @@ class ListsController extends Controller
 
     public function getIndex(Request $request)
     {
+        // PAGINATION
+        $page       = (int)$request->get('page', self::DEFAULT_PAGE) < 1 ? self::DEFAULT_PAGE : (int)$request->get('page', self::DEFAULT_PAGE);
+        $perPage    = (int)$request->get('per_page', self::PER_PAGE) < 1 ? self::PER_PAGE : (int)$request->get('per_page', self::PER_PAGE);
+        $offset     = ($page  - 1) * $perPage;
+
         $member = Auth::user();
 
         $parentID = $request->get('parent_id', 0);
 
-        $dirs = $member->directories()->where('parent_id', $parentID)->paginate();
+        $dirs = Directory::where('member_id', $member->id)
+                ->where('parent_id', $parentID)
+                ->select([
+                    DB::raw('"dir" as type'),
+                    'id as directory_id',
+                    DB::raw('NULL as upload_id'),
+                    DB::raw('parent_id'),
+                    'name',
+                    DB::raw('NULL as info'),
+                    'created_at',
+                    'updated_at',
+                ]);
 
-        $resp = $this->responseData($dirs, false, Constant::SUCCESS_TO_FETCH_ITEM);
+        $files = Upload::where('member_id', $member->id)
+                ->where('directory_id', $parentID)
+                ->select([
+                    DB::raw('"file" as type'),
+                    DB::raw('directory_id as directory_id'),
+                    'id as upload_id',
+                    DB::raw('NULL as parent_id'),
+                    'filename as name',
+                    DB::raw('info'),
+                    'created_at',
+                    'updated_at',
+                ]);
+
+        // union the queries
+        $dirs->unionAll($files);
+
+        $count = DB::table( DB::raw("({$dirs->toSql()}) as sub") )
+            ->mergeBindings($dirs->getQuery())
+            ->count();
+
+        $data = $dirs->take($perPage)->offset($offset)->get();
+
+        $items = [];
+        foreach ($data as $key => $value) {
+            $items[$key] = $value->toArray();
+            $items[$key]['info'] = !empty($value['info']) ? json_decode($value['info']) : [];
+            $items[$key]['links'] = Link::where('upload_id', $value->upload_id)->get();
+        }
+
+        if(is_array($items)){
+            $items = collect($items);
+        }
+
+        $res = new LengthAwarePaginator(
+            $items,
+            $count,
+            $perPage,
+            Paginator::resolveCurrentPage(),
+            ['path' => Paginator::resolveCurrentPath()]
+        );
+
+        $resp = $this->responseData($res, false, Constant::SUCCESS_TO_FETCH_ITEM);
 
         return response($resp);
     }
