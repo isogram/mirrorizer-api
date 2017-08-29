22e34c618ccae6248589bda6a4c716bcb5277a01
Muhammad Shidiq <emshidiq@gmail.com>
2017-08-30 01:12:40 +0700

    Update queue logic

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -154,7 +154,7 @@ class UploadController extends Controller
             try {            
 
                 // move the file
-                $theFile = time() . '_' . $strID . '_' . $fileName;
+                $theFile = $strID . '_' . $fileName;
                 $file->move($destinationPath, $theFile);
 
             } catch (\Exception $e) {
@@ -187,7 +187,9 @@ class UploadController extends Controller
 
                 // mirrorize here
                 // put jobs into queue to mirroring files
-                dispatch(new MirrorizerJob($upload));
+                foreach ($providers as $provider) {
+                    dispatch(new MirrorizerJob($upload, $provider));
+                }
 
             } catch (\Exception $e) {
 
